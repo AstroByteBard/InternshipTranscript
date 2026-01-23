68552940ccf1c9d9f82d4662a52497dc71a2a6c3
Napus Samuanpho <napus.dev@gmail.com>
2026-01-23 14:01:56 +0700

    email model

--- backend/server/Project/Academic/Academic.routes.js
+++ backend/server/Project/Academic/Academic.routes.js
@@ -5,7 +5,7 @@ const major = require("./service/major");
 const school = require("./service/school");
 
 router.get("/major", major.onQuerys);
-router.post("/major/explorers", major.onQuery);
+router.post("/major/sendmail", major.onSendEmail);
 router.post("/major", major.onCreate);
 router.put("/major", major.onUpdate);
 router.delete("/major", major.onDelete);
