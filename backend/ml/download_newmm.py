eed356574f3169506c815c7e31509bdd03b21392
Wasan Nachai <149573377+WasanNachai@users.noreply.github.com>
2026-04-21 02:19:34 +0700

    delete ML

--- backend/ml/download_newmm.py
+++
@@ -1,4 +0,0 @@
-from pythainlp.corpus import download
-
-download("sentence")
-print("Downloaded 'sentence' corpus for newmm tokenizer.")
--- backend/ml/force_download_newmm.py
+++
@@ -1,4 +0,0 @@
-from pythainlp.corpus import download
-
-download("sentence", force=True)
-print("Forced download of 'sentence' corpus for newmm tokenizer.")
